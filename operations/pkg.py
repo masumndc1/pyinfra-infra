83fab6e1fa462d658e7ab7816c5af5868c73982a
Khabir Uddin <khuddin@csc.fi>
2026-04-03 18:07:37 +0300

    modified pkg operation file

--- operations/pkg.py
+++ operations/pkg.py
@@ -2,47 +2,38 @@ import time
 from pyinfra import logger, host
 
 from pyinfra.operations import apt, yum, zypper
-from pyinfra.facts.server import LinuxName
-# from pyinfra.facts.server import LinuxDistribution
+from pyinfra.facts.server import LinuxDistribution
 
-true = yes = True
 start = time.time()
-distro = host.get_fact(LinuxName)
-packages = ["htop", "vim", "snapd"]
-# distro = host.get_fact(LinuxDistribution)
+distro = host.get_fact(LinuxDistribution)
+os_name = distro["name"]
+packages = host.data.get("extra_pkgs", []) + host.data.get("common_pkgs", [])
 
 # Update package list and install packages
-if distro in ["Debian", "Ubuntu"]:
+if os_name in ["Debian", "Ubuntu"]:
     apt.packages(
-        name="Install vim, snapd, and htop",
-        packages=packages,
-        # packages that comes from hosts or group vars
-        # packages=host.data.packages,
-        update=True,
-        _sudo=yes,
-    )
-
-elif distro in ["CentOS", "RedHat", "AlmaLinux"]:
-    yum.packages(
-        name="Install vim, snapd, and htop",
+        name="Install packages",
         packages=packages,
         update=True,
         _sudo=True,
     )
 
-elif distro in ["openSUSE Tumbleweed"]:
+elif os_name in ["CentOS", "RedHat", "AlmaLinux"]:
+    yum.packages(
+        name="Install packages",
+        packages=packages,
+        update=True,
+        _sudo=True,
+    )
+
+elif os_name in ["openSUSE Tumbleweed"]:
     zypper.packages(
-        name="Install vim, and htop",
-        packages=["htop", "vim"],
+        name="Install packages",
+        packages=packages,
         update=True,
         latest=True,
         _sudo=True,
     )
-
-# Using .get() for optional data
-# if host.data.get("install_nginx"):
-# Run nginx operations...
-# pass
 
 duration = time.time() - start
 logger.info(f"Installation took {time.time() - start:.2f}s")
