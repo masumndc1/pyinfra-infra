4eb8661f385cdfb7494964f24f975ecf76005e79
Khabir Uddin <khuddin@csc.fi>
2026-04-03 23:42:41 +0300

    run operation based on single host

--- deploy.py
+++ deploy.py
@@ -1,7 +1,10 @@
 from pyinfra import local, host
 
 # Include other operation files
-local.include("operations/pkg.py")
+# local.include("operations/pkg.py")
 
-if "web_server" in host.groups:
-    local.include("operations/nginx.py")
+# if "web_server" in host.groups:
+#    local.include("operations/nginx.py")
+
+if "sys-alma9-dev1" in host.name:
+    local.include("operations/pkg.py")
